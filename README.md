e6ce026b90c617fa79631945da3e3355c4a6ea98
Alexander Banks <alexbanks097@gmail.com>
2021-03-13 21:27:28 +0100

    website live

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ I'm currently finishing my master's degree in computer science with a speializat
 My thesis is on remote attestation of IoT devices in using Rust ⚡ 
 
 ### What am I learning? 📚
-Recently I have been learning Rust for my thesis. 
+Recently I have been learning Rust for my thesis, but I have also been playing around with servless functions in Firebase, as well as Flutter for Web!
 
 
 ### What am I passionate about? 🚀
-I am a bit of a Flutter fanatic 😅 I believe that it will revolutionize web development in the coming years!
+I am a bit of a Flutter fanatic 😅 I believe that it will revolutionize web development in the coming years! 
+By the way, check out my website 👉🏽 [banksdev](https://banksdev.dk) - it is built with Flutter 😎
 
 My most comfortable programming language is C# due to the vast experience I have in the .NET world, but currently my favorite programming language is 
 
--- banksdev/lib/main.dart
+++ banksdev/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:html' as html;
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,29 +11,63 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Frontpage(),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return OrientationBuilder(builder: (context, orientation) {
+        SizerUtil().init(constraints, orientation);
+        return MaterialApp(
+          theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Raleway"),
+          home: Frontpage(),
+        );
+      });
+    });
   }
 }
 
 class Frontpage extends StatelessWidget {
+  final iconButtons = [
+    IconButton(
+        iconSize: 100,
+        icon: FaIcon(FontAwesomeIcons.github),
+        onPressed: () =>
+            html.window.open("https://github.com/banksdev", "Github")),
+    IconButton(
+        iconSize: 100,
+        icon: FaIcon(FontAwesomeIcons.instagram),
+        onPressed: () =>
+            html.window.open("https://instagram.com/apexicph", "Github")),
+    IconButton(
+        iconSize: 100,
+        icon: FaIcon(FontAwesomeIcons.linkedin),
+        onPressed: () => html.window
+            .open("https://www.linkedin.com/in/banksdev", "LinkedIn")),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
+    var orientation = MediaQuery.of(context).orientation;
+    var isLandscape = orientation == Orientation.landscape;
 
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+    var socials = isLandscape
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: iconButtons)
+        : Column(
+            children: iconButtons,
+          );
+
+    return Scaffold(
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       CircleAvatar(
-          backgroundImage: Image.asset("assets/profile.jpg").image,
-          radius: screenSize.width < 1000 ? 100 : 250),
+          backgroundImage: Image.asset("assets/images/profile.jpg").image,
+          radius: isLandscape ? 15.0.w : 25.0.w),
       SizedBox(height: 50),
-      Text(
+      Center(
+          child: Text(
         "Hi, I'm Alexander, but you can call me Banks",
-        style: TextStyle(color: Colors.black),
-      )
-    ]);
+        style: TextStyle(fontSize: 14.0.sp, color: Colors.black),
+      )),
+      SizedBox(height: 50),
+      Padding(padding: EdgeInsets.fromLTRB(100, 0, 100, 0), child: socials)
+    ]));
   }
 }
