e9a0f23fafc3ad5a3f1c8fa1aa9242558c262a5c
Alexander Banks <alexbanks097@gmail.com>
2021-03-15 11:14:51 +0100

    done

--- banksdev/lib/main.dart
+++ banksdev/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:html' as html;
-import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return OrientationBuilder(builder: (context, orientation) {
-        SizerUtil().init(constraints, orientation);
-        return MaterialApp(
-          theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Raleway"),
-          home: Frontpage(),
-        );
-      });
-    });
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Raleway"),
+      home: Frontpage(),
+    );
   }
 }
 
@@ -56,18 +50,25 @@ class Frontpage extends StatelessWidget {
           );
 
     return Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      CircleAvatar(
-          backgroundImage: Image.asset("images/profile.jpg").image,
-          radius: isLandscape ? 15.0.w : 25.0.w),
-      SizedBox(height: 50),
-      Center(
-          child: Text(
-        "Hi, I'm Alexander, but you can call me Banks",
-        style: TextStyle(fontSize: 14.0.sp, color: Colors.black),
-      )),
-      SizedBox(height: 50),
-      Padding(padding: EdgeInsets.fromLTRB(100, 0, 100, 0), child: socials)
-    ]));
+        body: Center(
+            child: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+          SizedBox(height: isLandscape ? 0 : 10),
+          CircleAvatar(
+            backgroundImage: Image.asset("assets/images/profile.jpg").image,
+            radius: isLandscape ? 200 : 150,
+          ),
+          SizedBox(height: 50),
+          Center(
+              child: Text(
+            "Hi, I'm Alexander, but you can call me Banks",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 28.0, color: Colors.black),
+          )),
+          SizedBox(height: 50),
+          Padding(padding: EdgeInsets.fromLTRB(100, 0, 100, 0), child: socials)
+        ]))));
   }
 }
